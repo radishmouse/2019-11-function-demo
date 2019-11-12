13c73dd57cb05fde2940d4c6db93939aed0738a7
Chris Aquino <chrisaquino@radishmouse.local>
2019-11-12 13:48:15 -0500

    adds greet() function example with notes

--- hello.py
+++ hello.py
@@ -1,1 +1,21 @@
-print("Hayyyy y'all")
+
+# How do I define a function?
+def greet(whom):
+    print(f"Hayyyy {whom.upper()}")
+
+
+# cats = ["Oakley", "Milla"]
+# for cat in cats:
+#     # print(f"Hayyyy {cat.upper()}")
+#     greet(cat)
+
+# How do I use a function?
+greet("Bob")
+# We "call" the function by typing its name and passing it any arguments it needs.
+# Another way to say this is: we "invoke" the function.
+
+# Are arguments required?
+#greet()
+# Yes, they are required if there were arguments included in the definition of the function.
+
+# Can I access arguments outside the body of a function?
